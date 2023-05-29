c4403868b009f39f8c65a2fd4c42862a81f365a5
Sasa M <smikodanic@gmail.com>
2023-05-29 13:00:20 +0200

    fix

--- server/index.js
+++ server/index.js
@@ -1,33 +1,10 @@
 import { HTTPServer } from '@mikosoft/spa-server';
 
 
-///// development HTTP Server
-/*
-const httpOpts = {
-  staticDir: 'src',
-  indexFile: 'index.html',
-  openDirs: 'node_modules/@mikosoft/dodo',
-  port: process.env.PORT || 3001,
-  timeout: 5 * 60 * 1000, // if 0 never timeout
-  acceptEncoding: 'gzip', // gzip, deflate or ''
-  headers: {
-    // CORS Headers
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    'Access-Control-Allow-Methods': 'GET', // 'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, HEAD',
-    'Access-Control-Max-Age': '3600'
-  },
-  debug: false
-};
-const httpServer = new HTTPServer(httpOpts);
-httpServer.start();
-*/
-
-
 const httpOpts = {
   staticDir: 'dist',
   indexFile: 'index.html',
-  openDirs: 'src/views',
+  openDirs: '',
   port: process.env.PORT || 3000,
   timeout: 5 * 60 * 1000, // if 0 never timeout
   acceptEncoding: 'gzip', // gzip, deflate or ''
@@ -38,7 +15,7 @@ const httpOpts = {
     'Access-Control-Allow-Methods': 'GET', // 'Access-Control-Allow-Methods': 'GET, POST, PUT, PATCH, DELETE, HEAD',
     'Access-Control-Max-Age': '3600'
   },
-  debug: true
+  debug: false
 };
 const httpServer = new HTTPServer(httpOpts);
 httpServer.start();
