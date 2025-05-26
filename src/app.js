d7e369a27d4f8961eb0a99e630756e87c42aeea4
Sasa M <smikodanic@gmail.com>
2025-05-26 18:25:44 +0200

    one page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,37 @@
-import { AppOne } from '@mikosoft/dodo';
-
-// conf
+import { AppOne, Controller, corelib } from '@mikosoft/dodo';
 import { $httpClient, $debugOpts } from './conf/index.js';
 
+
 // controller
-import HomeCtrl from './controllers/HomeCtrl.js';
+class HomeCtrl extends Controller {
+
+  constructor(app) {
+    super();
+  }
+
+  // trx argument is undefined because there's no router
+  async __loader() {
+    this.setTitle('AppOne - DoDo Framework');
+    this.setDescription('DoDo is JS framework for one page applications.');
+    this.setKeywords('dodo, framework, javascript, js, single page app, spa');
+    this.setLang('en');
+    this.loadSelfview();
+  }
+
+  async __init() {
+    console.log('init() -- ctrl::', this);
+    await this.fetchPosts();
+  }
+
+  async __postrend() {
+  }
+
+  async fetchPosts() {
+    const answer = await this.$httpClient.askJSON('https://jsonplaceholder.typicode.com/posts');
+    this.$model.posts = answer.res.content || [];
+  }
+
+}
 
 
 // app
